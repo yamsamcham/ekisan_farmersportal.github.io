2de22045c7e24dc889541e6384d6f8776c101d17
yamsamcham <yamsamcham3@gmail.com>
2021-06-10 10:59:02 +0530

    try heroku

--- ekisan/settings.py
+++ ekisan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +143,6 @@ EMAIL_HOST_PASSWORD = 'ekisanmini'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+
+django_heroku.settings(locals())
